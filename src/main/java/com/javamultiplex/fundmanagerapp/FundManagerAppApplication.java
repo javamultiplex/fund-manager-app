8cd8781d79332db678c7f081c436081eb25538d3
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-11-17 06:27:56 +0530

    added azure plugin

--- src/main/java/com/javamultiplex/fundmanagerapp/FundManagerAppApplication.java
+++ src/main/java/com/javamultiplex/fundmanagerapp/FundManagerAppApplication.java
@@ -2,12 +2,19 @@ package com.javamultiplex.fundmanagerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FundManagerAppApplication {
+public class FundManagerAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundManagerAppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FundManagerAppApplication.class);
 	}
 
 }
